71517747db01dddb0faab4063d8c5761ffc04cb9
Ansgar Mertens <ansgar@hashicorp.com>
2023-03-08 10:16:18 +0100

    chore: configure projen to run copywrite after build on PRs

--- .projenrc.ts
+++ .projenrc.ts
@@ -39,4 +39,14 @@ project.testTask.exec(`npx cdktf synth`, {
 project.gitignore.include("examples/typescript/tsconfig.json");
 project.jest?.addIgnorePattern("dist");
 
+// Run copywrite tool to add copyright headers to all files
+project.buildWorkflow?.addPostBuildSteps(
+  {
+    name: "Setup Copywrite tool",
+    uses: "hashicorp/setup-copywrite@3ace06ad72e6ec679ea8572457b17dbc3960b8ce", // v1.0.0
+    with: { token: "${{ secrets.GITHUB_TOKEN }}" },
+  },
+  { name: "Add headers using Copywrite tool", run: "copywrite headers" }
+);
+
 project.synth();
