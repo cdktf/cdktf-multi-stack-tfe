d77a0090d053c11b275dad394317e6b4ee2b50c6
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-03-08 09:46:54 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 import { ConstructLibraryCdktf } from "projen/lib/cdktf";
 const cdktfVersion = ">=0.15.0";
 const constructVersion = "^10.0.107";
--- test/multi-stack-tfe.test.ts
+++ test/multi-stack-tfe.test.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 import { TerraformLocal, Testing } from "cdktf";
 import { Construct } from "constructs";
 import { BaseStack, Stack, WorkspaceConfig, TFVariable } from "../src";
